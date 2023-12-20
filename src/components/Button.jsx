38f18bb3852030f0577f471a8b7e7dc3a2c2f71b
doctor <fd5@MacBook-Air-2022.local>
2023-12-20 16:24:49 +0700

    done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,38 @@
-// Start coding here
+/**@jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <div
+        css={css`
+          width: 50px;
+          color: white;
+          background-color: #074ee8;
+          border-radius: 4px;
+          padding: 0 16px;
+          display: inline-flex;
+        `}
+      >
+        {props.text}
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div
+        css={css`
+          width: 50px;
+          color: white;
+          background-color: #07a4e8;
+          border-radius: 4px;
+          padding: 0 16px;
+          display: inline-flex;
+        `}
+      >
+        {props.text}
+      </div>
+    );
+  }
+}
+
+export default Button;
